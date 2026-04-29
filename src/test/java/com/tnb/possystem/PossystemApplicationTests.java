4695b96368a291b83dcc3bf77dd879c3af975855
Tubanur Balci <tubanurbalci5015@gmail.com>
2026-04-29 19:23:47 +0300

    project setup

---
+++ src/test/java/com/tnb/possystem/PossystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnb.possystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PossystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

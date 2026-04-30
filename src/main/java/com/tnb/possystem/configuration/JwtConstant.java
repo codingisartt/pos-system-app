d5a30406c33b2750112aff87320781681f62ae53
Tubanur Balci <tubanurbalci5015@gmail.com>
2026-04-30 15:24:21 +0300

    feat: implement JWT authentication system
    - add SecurityConfig for request filtering
    - implement JwtProvider and JwtValidator
    - define JwtConstant values

---
+++ src/main/java/com/tnb/possystem/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.tnb.possystem.configuration;
+
+public class JwtConstant {
+    public static final String JWT_SECRET = "jfjsdkafkafkskgrkkrfiijvnsevaivskvnskfegffakcnzmcmcsfuafhuerhfwihuehhuefhefjvcnzjncmzcnmnjdwereofprkforjiivsdjcnjcnsdjcdbscjzbbdb";
+    public static final String JWT_HEADER = "Authorization";
+}
